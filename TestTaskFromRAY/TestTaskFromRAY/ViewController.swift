428d820ced45091d60d70f2de57a7a17a4154646
Oleg Kalistratov <oleg.K@northitgroup.eu>
2023-05-30 13:05:56 +0300

    initial project

---
+++ TestTaskFromRAY/TestTaskFromRAY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskFromRAY
+//
+//  Created by Oleg Kalistratov on 30.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
