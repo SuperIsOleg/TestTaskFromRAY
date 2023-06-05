be168f65fca708957c2ad7ae633bf22ff041a7ce
Oleg Kalistratov <oleg.K@northitgroup.eu>
2023-06-05 10:30:57 +0300

    add testDeleteImageModel

--- TestTaskFromRAY/TestTaskFromRAYTests/TestTaskFromRAYTests.swift
+++ TestTaskFromRAY/TestTaskFromRAYTests/TestTaskFromRAYTests.swift
@@ -10,14 +10,14 @@ import XCTest
 @testable import TestTaskFromRAY
 
 class TestTaskFromRAYTests: XCTestCase {
-
+    
     var imageCoreDataManager: ImageCoreDataManagerProtocol!
     
     override func setUp() {
         super.setUp()
         imageCoreDataManager = ImageCoreDataManager.shared
     }
-
+    
     override func tearDown() {
         imageCoreDataManager = nil
         super.tearDown()
@@ -38,10 +38,40 @@ class TestTaskFromRAYTests: XCTestCase {
                 XCTAssertEqual(createdImage.imageData, imageData, "Данные должны быть одинаковыми")
             }
         case .failure(let error):
-            XCTFail("Получение изображений не должно упасть\nОшибка: \(error.localizedDescription)")
+            XCTFail(error.localizedDescription)
         }
     }
-
+    
+    func testDeleteImageModel() {
+        let imageUrl = URL(string: "https://example.com/image.jpg")!
+        let imageData = Data("testImageData".utf8)
+        
+        imageCoreDataManager.createImageModel(data: imageData, imageUrl: imageUrl)
+        
+        let imagesResult = imageCoreDataManager.getAllImages()
+        var initialCount = 0
+        
+        switch imagesResult {
+        case .success(let images):
+            initialCount = images.count
+            if let imageToDelete = images.first {
+                imageCoreDataManager.deleteImageModel(item: imageToDelete)
+            } else {
+                XCTFail("Должно быть хотя бы одно изображение для удаления")
+            }
+        case .failure(let error):
+            XCTFail(error.localizedDescription)
+        }
+        
+        let imagesAfterDeletionResult = imageCoreDataManager.getAllImages()
+        switch imagesAfterDeletionResult {
+        case .success(let imagesAfterDeletion):
+            XCTAssertEqual(imagesAfterDeletion.count, initialCount - 1, "Количество изображений должно уменьшиться на 1")
+        case .failure(let error):
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
     func testPerformanceExample() throws {
         self.measure { }
     }
